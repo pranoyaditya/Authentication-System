1b973f10a52e113f138d969a72f695a9bb810604
pranoyaditya <pranoyaditya@gmail.com>
2024-12-03 18:51:17 +0600

    completed signUp view funtion, model forms and signup page

--- authentication_system/user_app/views.py
+++ authentication_system/user_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,LoginForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate,update_session_auth_hash
+from django.contrib import messages
 
 # Create your views here.
+def signUp(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully.')
+            return redirect('user_login')
+    return render(request, 'user_app/signup.html', {'form':form})
