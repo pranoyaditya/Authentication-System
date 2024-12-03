57dfffbf5084f909f21eb84862692e18326bd4a0
pranoyaditya <pranoyaditya@gmail.com>
2024-12-03 18:29:02 +0600

    completed partially base.html and home page front-end part design and added signup url in  user app urls.

--- authentication_system/user_app/urls.py
+++ authentication_system/user_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_login, name='user_logout'),
+    path('sign_up/', views.signUp, name='user_signUp')
     path('', views.profile, name='profile'),
 ]
